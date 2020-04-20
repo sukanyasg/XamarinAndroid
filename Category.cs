fc856768aecec17cae533ae4f3fc9c1e45816509
Sukanya Sengupta <7634917+sukanyasg@users.noreply.github.com>
2020-04-20 18:12:17 -0500

    Added exercise files

---
+++ Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopMobile.Core.Model
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
