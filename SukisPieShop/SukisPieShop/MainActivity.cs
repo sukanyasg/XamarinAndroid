d1763e5ccdd2f3da18c11f51989a2827c0351f93
Sukanya Sengupta <sukanya.sengupta@wellspring.com>
2020-05-05 15:12:02 -0500

    Added solution till part 3

---
+++ SukisPieShop/SukisPieShop/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace SukisPieShop
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Button myButton = FindViewById<Button>(Resource.Id.MyButton);
+            myButton.Click += MyButton_Click;
+        }
+
+        private void MyButton_Click(object sender, System.EventArgs e)
+        {
+            var toast = Toast.MakeText(this, "A button was clicked", ToastLength.Short);
+            toast.Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
